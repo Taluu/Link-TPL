1bf6a806a49851f17bdf294383a39ced455794fa
Baptiste "Talus" Clavie <clavie.b@gmail.com>
2010-06-09 12:54:10 +0200

    Reindentation des filtres

--- Talus_TPL/Filters.php
+++ Talus_TPL/Filters.php
@@ -32,181 +32,180 @@
  */
 
 final class Talus_TPL_Filters {
-    /**
-     * Arrondi la valeur donnée à l'entier supérieur
-     *
-     * @param string $arg
-     * @return string
-     */
-    public static function ceil($arg){
-        return (string) ceil((int) $arg);
-    }
-    
-    /**
-     * Arrondi la valeur à l'entier inférieur
-     *
-     * @param string $arg
-     * @return string
-     */
-    public static function floor($arg){
-        return (string) floor((int) $arg);
-    }
-    
-    /**
-     * Encode les caractères html spéciaux de la variable
-     *
-     * @param string $arg
-     * @return string
-     */
-    public static function protect($arg){
-        return htmlspecialchars($arg);
-    }
-    
-    /**
-     * Met le contenu de la variable en MAJUSCULE
-     *
-     * @param string $arg
-     * @return string
-     */
-    public static function capitalize($arg){
-        return mb_strtoupper($arg);
-    }
-    
-    /**
-     * Met le contenu de la variable en minuscule
-     *
-     * @param string $arg
-     * @return string
-     */
-    public static function minimize($arg){
-        return mb_strtolower($arg);
-    }
-    
-    /**
-     * Met la première lettre d'une variable en Majuscule
-     *
-     * @param string $arg
-     * @return string
-     */
-    public static function ucfirst($arg){
-        $arg[0] = mb_strtoupper($arg[0]);
+  /**
+   * Arrondi la valeur donnée à l'entier supérieur
+   *
+   * @param string $arg
+   * @return string
+   */
+  public static function ceil($arg){
+    return (string) ceil((int) $arg);
+  }
 
-        return $arg;
-    }
-    
-    /**
-     * Met la première lettre d'une variable en minuscule
-     *
-     * @param string $arg
-     * @return string
-     */
-    public static function lcfirst($arg){
-        $arg[0] = mb_strtolower($arg[0]);
-        
-        return $arg;
-    }
-    
-    /**
-     * Met la première lettre de chaques mots d'une variable en Majuscule
-     *
-     * @param string $arg
-     * @return string
-     */
-    public static function ucwords($arg){
-        return mb_convert_case($arg, MB_CASE_TITLE);
-    }
-    
-    /**
-     * Change la casse d'une variable
-     *
-     * @param string $arg
-     * @return string
-     */
-    public static function invertCase($arg){
-        for ($i = 0, $length = mb_strlen($arg); $i < $length; $i++){
-            $tolower = mb_strtolower($arg[$i]);
-            $arg[$i] = $arg[$i] == $tolower ? mb_strtoupper($arg[$i]) : $tolower;
-        }
-        
-        return $arg;
-    }
-    
-    /**
-     * Transforme les sauts de lignes en <br />
-     *
-     * @param string $arg
-     * @return string
-     */
-    public static function nl2br($arg){
-        return nl2br($arg);
+  /**
+   * Arrondi la valeur à l'entier inférieur
+   *
+   * @param string $arg
+   * @return string
+   */
+  public static function floor($arg){
+    return (string) floor((int) $arg);
+  }
+
+  /**
+   * Encode les caractères html spéciaux de la variable
+   *
+   * @param string $arg
+   * @return string
+   */
+  public static function protect($arg){
+    return htmlspecialchars($arg);
+  }
+
+  /**
+   * Met le contenu de la variable en MAJUSCULE
+   *
+   * @param string $arg
+   * @return string
+   */
+  public static function capitalize($arg){
+    return mb_strtoupper($arg);
+  }
+
+  /**
+   * Met le contenu de la variable en minuscule
+   *
+   * @param string $arg
+   * @return string
+   */
+  public static function minimize($arg){
+    return mb_strtolower($arg);
+  }
+
+  /**
+   * Met la première lettre d'une variable en Majuscule
+   *
+   * @param string $arg
+   * @return string
+   */
+  public static function ucfirst($arg){
+    $arg[0] = mb_strtoupper($arg[0]);
+
+    return $arg;
+  }
+
+  /**
+   * Met la première lettre d'une variable en minuscule
+   *
+   * @param string $arg
+   * @return string
+   */
+  public static function lcfirst($arg){
+    $arg[0] = mb_strtolower($arg[0]);
+
+    return $arg;
+  }
+
+  /**
+   * Met la première lettre de chaques mots d'une variable en Majuscule
+   *
+   * @param string $arg
+   * @return string
+   */
+  public static function ucwords($arg){
+    return mb_convert_case($arg, MB_CASE_TITLE);
+  }
+
+  /**
+   * Change la casse d'une variable
+   *
+   * @param string $arg
+   * @return string
+   */
+  public static function invertCase($arg){
+    for ($i = 0, $length = mb_strlen($arg); $i < $length; $i++){
+      $tolower = mb_strtolower($arg[$i]);
+      $arg[$i] = $arg[$i] == $tolower ? mb_strtoupper($arg[$i]) : $tolower;
     }
 
-    /**
-     * Transforme la chaine de caractère en chaine slug
-     *
-     * @param string $arg Argument à slugifier
-     * @example "Bernard au Marché" => "bernard-au-marche"
-     * @link http://www.symfony-project.org/jobeet/ Méthode Jobeet::Slugify()
-     * @return string
-     */
-    static public function slugify($arg) {
-      $arg = preg_replace('~[^\\pL\d]+~u', '-', $arg);
-      $arg = trim($arg, '-');
+    return $arg;
+  }
 
-      if (function_exists('iconv')) {
-        $arg = iconv('utf-8', 'us-ascii//TRANSLIT', $arg);
-      }
+  /**
+   * Transforme les sauts de lignes en <br />
+   *
+   * @param string $arg
+   * @return string
+   */
+  public static function nl2br($arg){
+    return nl2br($arg);
+  }
 
-      $arg = strtolower($arg);
-      $arg = preg_replace('~[^-\w]+~', '', $arg);
+  /**
+   * Créé le slug du nom de l'objet, et le renvoi
+   * Méthode venant du projet Jobeet par le tutoriel Symfony
+   *
+   * @link http://www.symfony-project.org Framework Symfony
+   * @return string
+   */
+  static public function slugify($arg) {
+    $arg = preg_replace('~[^\\pL\d]+~u', '-', $arg);
+    $arg = trim($arg, '-');
 
-      if (!$arg) {
-        $arg = 'n-a';
-      }
+    if (function_exists('iconv')) {
+      $arg = iconv('utf-8', 'us-ascii//TRANSLIT', $arg);
+    }
 
+    $arg = strtolower($arg);
+    $arg = preg_replace('~[^-\w]+~', '', $arg);
+
+    if (!$arg) {
+      $arg = 'n-a';
+    }
+
+    return $arg;
+  }
+
+  /**
+   * Coupe une chaine de caractères (sans interrompre un mot)
+   *
+   * @param string $arg chaine à couper
+   * @param integer $max nombre maximum de caractères
+   * @param string $finish chaine de caractère à appliquer en fin si $str est coupée.
+   * @return string
+   */
+  public static function cut($arg, $max = 50, $finish = '...'){
+    if (strlen($arg) <= $max){
       return $arg;
     }
 
-    /**
-     * Coupe une chaine de caractères (sans interrompre un mot)
-     *
-     * @param string $arg chaine à couper
-     * @param integer $max nombre maximum de caractères
-     * @param string $finish chaine de caractère à appliquer en fin si $str est coupée.
-     * @return string
-     */
-    public static function cut($arg, $max = 50, $finish = '...'){
-      if (strlen($arg) <= $max){
-        return $arg;
-      }
+    $max = intval($max) - strlen($finish);
 
-      $max = intval($max) - strlen($finish);
+    $arg = substr($arg, 0, $max + 1);
+    $arg = strrev(strpbrk(strrev($arg), " \t\n\r\0\x0B"));
 
-      $arg = substr($arg, 0, $max + 1);
-      $arg = strrev(strpbrk(strrev($arg), " \t\n\r\0\x0B"));
+    return rtrim($arg) . $finish;
+  }
 
-      return rtrim($arg) . $finish;
+  /**
+   * Transforme les sauts de lignes intelligement
+   * Deux sauts de lignes font un paragraphe (<p>), et un saut de ligne donne un <br />
+   * Adapté de python à php d'après le package "utils.html" de Django.
+   *
+   * @param string $arg Texte à parser
+   * @return string
+   */
+  public static function linebreaks($arg){
+    $arg = str_replace(array("\r\n", "\r"), "\n", $arg);
+
+    $paras = preg_split("`\n{2,}`si", $arg);
+
+    foreach ($paras as &$para) {
+      $para = str_replace("\n", '<br />' . "\n", $para);
     }
 
-    /**
-     * Transforme les sauts de lignes intelligement
-     * Deux sauts de lignes font un paragraphe (<p>), et un saut de ligne donne un <br />
-     * Adapté de python à php d'après le package "utils.html" de Django.
-     *
-     * @param string $arg Texte à parser
-     * @return string
-     */
-    public static function linebreaks($arg){
-      $arg = str_replace(array("\r\n", "\r"), "\n", $arg);
-      
-      $paras = preg_split("`\n{2,}`si", $arg);
-
-      foreach ($paras as &$para) {
-        $para = str_replace("\n", '<br />' . "\n", $para);
-      }
-
-      return '<p>' . implode('</p>' . PHP_EOL . PHP_EOL . '<p>', $paras) . '</p>';
-    }
+    return '<p>' . implode('</p>' . PHP_EOL . PHP_EOL . '<p>', $paras) . '</p>';
+  }
 }
 
 /** EOF /**/
