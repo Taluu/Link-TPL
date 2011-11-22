3ab971de440925adaaa3f63bf8171e0a65068cc0
Baptiste "Talus" Clavie <clavie.b@gmail.com>
2011-11-22 23:37:04 +0100

    forgot the Cache exception file -- cleaning exceptions, part 2

---
+++ lib/Link/Exceptions/Cache.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of Link TPL.
+ * 
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @copyright Copyleft (c) 2007+, Baptiste Clavié, Talus' Works
+ * @link http://www.talus-works.net Talus' Works
+ * @license http://www.opensource.org/licenses/BSD-3-Clause Modified BSD License
+ * @version $Id$
+ */
+
+/**
+ * Cache exceptions
+ * 
+ * These exceptions are sent when there is an error with the cache.
+ * 
+ * @package Link
+ * @author Baptiste "Talus" Clavié <clavie.b@gmail.com>
+ */
+class Link_Exceptions_Cache extends Link_Exception {}
+
+/*
+ * EOF
+ */
