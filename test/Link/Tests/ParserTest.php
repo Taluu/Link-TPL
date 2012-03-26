b5c1e1bffc8ad8be944170045d03d703101e75e1
Baptiste "Talus" Clavie <clavie.b@gmail.com>
2012-03-26 17:40:51 +0200

    Added the structure to test the parser

---
+++ test/Link/Tests/ParserTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of Link TPL.
+ * 
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @copyright Copyleft (c) 2007+, Baptiste Clavié, Talus' Works
+ * @link http://www.talus-works.net Talus' Works
+ * @license http://www.opensource.org/licenses/BSD-3-Clause Modified BSD License
+ * @version $Id$
+ */
+
+/**
+ * Tests the templates' parser
+ * 
+ * @package Link.test
+ * @author Baptiste "Talus" Clavié <clavie.b@gmail.com>
+ *  
+ */
+class Link_Tests_ParserTest extends PHPUnit_Framework_TestCase {
+  public function testConditions() {
+    $this->markTestIncomplete('Not implemented yet :(');
+  }
+  
+  public function testLoops() {
+    $this->markTestIncomplete('Not implemented yet :(');
+  }
+  
+  public function testVars() {
+    $this->markTestIncomplete('Not implemented yet :(');
+  }
+  
+  public function testIncludes() {
+    $this->markTestIncomplete('Not implemented yet :(');
+  }
+}
