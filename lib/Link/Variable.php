104200212ec765609f55f5b82a3ad1fedc181527
Baptiste "Talus" Clavie <clavie.b@gmail.com>
2013-01-10 00:26:30 +0100

    Add the [] case on offsetSet in Link_Variable

--- lib/Link/Variable.php
+++ lib/Link/Variable.php
@@ -40,12 +40,18 @@ class Link_Variable implements Link_VariableInterface {
     }
 
     /** {@inheritDoc} */
-    public function offsetSet($offset, $value) {
-        if (!$value instanceof self) {
-            $value = new self($this->getEnvironment(), $value);
+    public function offsetSet($offset = null, $value) {
+        if (!is_array($this->_value) && !$this->_value instanceof ArrayAccess && ('string' !== getType($this->_value) && 'integer' !== getType($offset))) {
+            throw new Link_Exception_Runtime('This variable is not an array, a traversable, or a string and a numeric offset');
         }
 
-        $this->_value[$offset] = $value;
+        if (null !== $offset) {
+            $this->_value[$offset] = $this->toSelf($value);
+
+            return;
+        }
+
+        $this->_value[] = $this->toSelf($value);
     }
 
     /** {@inheritDoc} */
